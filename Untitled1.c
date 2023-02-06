1cb16f0042080e49360d9d57459734f58d894015
20R01A0429 <20r01a0429@cmritonline.ac.in>
2023-02-06 20:55:22 +0530

    1

---
+++ Untitled1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+        int n;
+            scanf("%d",&n);
+            for(i=0;i<n;i++){
+                int n[100];
+            scanf("%d",&n[i]);
+            printf("%d",n[i]);}
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;}
